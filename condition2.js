f23130db2791529f77657705bcffe12bc4078fa0
Md-Sujon <mdsujon829562@gmail.com>
2021-10-14 16:59:42 +0600

    s

--- condition2.js
+++ condition2.js
@@ -1,7 +1,18 @@
 let StudyComplete = true;
-let Balance = 2000000;
-if(StudyComplete === true && Balance > 3000000){
+let Balance = 4000000;
+if(StudyComplete == true && Balance > 3000000){
        console.log('Ami BSC korbo');
+}else{
+    console.log('Kopal ar porasona nai');
+}
+
+
+let StudyComplete1 = true;
+let Balance1 = 5000000;
+if(StudyComplete1 == false && Balance1 > 3000000){
+       console.log('Ami BSC korbo');
+}else if(StudyComplete1 == true){
+console.log('Taka manage koro');
 }else{
     console.log('Kopal ar porasona nai');
 }
