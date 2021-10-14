d01545e99233aad4755effacba2f0a485971a38c
Md-Sujon <mdsujon829562@gmail.com>
2021-10-14 16:51:11 +0600

    condition

---
+++ Condition.js
@@ -0,0 +1,22 @@
+let number = 19;
+if(number%2==0){
+    console.log('This is Even number');
+}else{
+    console.log('This is Odd number');
+}
+
+let number1 = 20;
+if(number1 != 10){
+console.log('i will eat cha ruti');
+}else{
+    console.log('only cha');
+}
+
+let number3 = 10;
+if(number3 < 40 || number3 > 30){
+console.log('I it Bhat mac');
+}else if(number3 < 30 || number3 > 20) {
+    console.log('Only Khecuri');
+}else {
+    console.log('Only Ruti');
+} 
---
+++ condition2.js
@@ -0,0 +1,7 @@
+let StudyComplete = true;
+let Balance = 2000000;
+if(StudyComplete === true && Balance > 3000000){
+       console.log('Ami BSC korbo');
+}else{
+    console.log('Kopal ar porasona nai');
+}
