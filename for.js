5cc4d6364911af6e08be058485a8a1873d3bed0c
Md-Sujon <mdsujon829562@gmail.com>
2021-10-17 13:15:36 +0600

    switch

---
+++ for.js
@@ -0,0 +1,13 @@
+/* var number = [12,34,45,23,45,566,767,878,3423,453,213,13,324,4353,7665,765,78,879,56,34,324]
+
+for (var i = 0; i<number.length; i++){
+ var element = number[i];
+ console.log(element);
+}
+*/
+
+let friend = ['sujon', 'james', 'smita', 'mamun', 'rakib'];
+for(let i = 0; i < friend.length; i++){
+    let element = friend[i];
+    console.log(element);
+}
---
+++ switch.js
@@ -0,0 +1,49 @@
+
+/*
+var nam = 'sujon';
+
+switch (nam){
+    case 'smita':
+    console.log('I am smita');
+    break;
+
+    case 'Rakib':
+        console.log('i am Rakib');
+        break;
+
+    case 'susmita':
+        console.log('i am susmita');
+        break;
+     
+    case 'Alifa':
+        console.log('i am alifa');
+        break;   
+        
+        case 'kuk':
+            console.log('i am sujon');
+            break; 
+       default:
+           console.log('i am kao na');         
+}*/
+
+var num = 10;
+
+switch (num) {
+    case 1000:
+        console.log('i am 1000');
+        break; 
+        case 500:
+            console.log('i am 500');
+            break;
+
+        case 10:
+            console.log('i am 10');
+            break; 
+
+            case 100:
+                console.log('i am 100');
+                break;
+
+        default:
+            console.log('dur khuja pai na');
+}
