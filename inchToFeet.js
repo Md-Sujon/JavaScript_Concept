86986846517e7e2cbf353e190f62654f86cddad2
Md-Sujon <mdsujon829562@gmail.com>
2021-10-19 11:27:39 +0600

    m

---
+++ inchToFeet.js
@@ -0,0 +1,18 @@
+var inch = 156;
+var feet = inch / 12;
+console.log(feet);
+
+            function inchToFeet(convert){
+             var feet = convert / 12;
+               return feet;
+               }
+var output = inchToFeet(254554);
+console.log(output.toFixed(2)+' inch');
+var output1 = inchToFeet(4465486);
+console.log(output1.toFixed(2)+' inch');
+var output2 = inchToFeet(56546);
+console.log(output2.toFixed(2)+' inch');
+var output3 = inchToFeet(9685);
+console.log(output3.toFixed(2)+' inch');
+var output4 = inchToFeet(6454);
+console.log(output4.toFixed(2)+' inch');
