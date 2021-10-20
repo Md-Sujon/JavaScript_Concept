08afcd88054903427693834bfcf6e3c36bdea96b
Md-Sujon <mdsujon829562@gmail.com>
2021-10-20 11:54:26 +0600

    s

---
+++ factorialRecursive.js
@@ -0,0 +1,44 @@
+// 10! = 10*9*8*7*6*5*4*3*2*1;
+
+
+
+function factorialRe(num){
+    let fact = 1;
+    while(num >= 1){
+        fact = fact*num;
+         num--;
+    } return fact;
+}
+let output = factorialRe(10);
+console.log(output);
+
+console.log('**************************************');
+
+
+
+function factorialRecursive(n){
+    let fact = 1;
+    for (let i = n; i >=1; i--) {
+           fact *=i;
+    }return fact;
+}
+ 
+let output1 = factorialRecursive(10)
+console.log(output1);
+/*
+6! = 1*2*3*4*5*6;
+7!  = 1*2*3*4*5*6*7;
+7!  = (7-1)!*7;
+n!  = (n-1)!*n;
+*/
+
+function factorialRecursive(n){
+    if(n===0){
+        return 1;
+    }else{
+        return n*factorialRecursive(n-1)
+    }
+}
+
+let fact = factorialRecursive(10);
+console.log(fact);
