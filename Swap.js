6f97dbef03cd1e5e8db393418ac0f97a7c49d6d0
Md-Sujon <mdsujon829562@gmail.com>
2021-10-21 22:18:03 +0600

    Swap

--- Swap.js
+++ Swap.js
@@ -3,8 +3,8 @@ let b = 25;
 let c = a;
     a=b;
     b=c;
-    
 console.log('After swap','a =',a, 'b =',b);
+
 
 let x=8;
 let y=9;
@@ -12,9 +12,9 @@ let y=9;
   y=x-y;
   x=x-y;
    console.log('After swap','x =',x, 'y =',y);
+
+
 let p=6;
 let q=7;
 [p,q] = [q,p]
-
-   
-   console.log('After swap','p =',p, 'q =',q);
+console.log('After swap','p =',p, 'q =',q);
