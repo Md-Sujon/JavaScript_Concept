c3d352acd6b3ec2da0866397754675d0b9502ae1
Md-Sujon <mdsujon829562@gmail.com>
2021-10-21 23:13:18 +0600

    max

---
+++ max.js
@@ -0,0 +1,25 @@
+var a = 55540;
+var b = 72;
+var c = 90000000;
+
+var result =Math.max(a,b,c);
+var result2 =Math.min(a,b,c);
+console.log(result2);
+console.log(result);
+
+
+/*
+if(a>b){
+    if(a>c){
+        console.log(a);
+    }else{
+        console.log(c);
+    }
+}else{
+    if(b>c){
+        console.log(b);
+} else {
+    console.log(c);
+}
+}
+*/
