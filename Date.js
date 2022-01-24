7d29449734f41ef1b9f13f4a6b782da287068e8d
Md-Sujon <mdsujon829562@gmail.com>
2022-01-24 22:39:53 +0600

    random number

---
+++ Date.js
@@ -0,0 +1,15 @@
+var date = new Date();
+
+console.log(date); //2022-01-24T16:35:39.799Z
+
+console.log(date.toDateString()); //Mon Jan 24 2022
+
+console.log(date.toLocaleDateString()); // 1/24/2022
+
+console.log(date.toLocaleString()); // 1/24/2022, 10:35:39 PM
+
+console.log(date.toUTCString()); // Mon, 24 Jan 2022 16:37:34 GMT
+
+
+
+
