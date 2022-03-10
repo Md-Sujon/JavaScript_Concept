37b650855ad74315eae9d2522d3a7352d6750bca
Md-Sujon <mdsujon829562@gmail.com>
2022-03-10 23:26:18 +0600

    switch case

---
+++ NumberLessEqualZero.js
@@ -0,0 +1,5 @@
+function lessThanOrEqualToZero(number){
+return(number <= 0) ? true : false;
+}
+const log = lessThanOrEqualToZero(5);
+console.log(log);
---
+++ SwitchCase.js
@@ -0,0 +1,35 @@
+var date = new Date();
+var Today = date.getMonth();
+
+
+switch (8) {
+    case 0 :
+    console.log('Today is sunday');
+    break;
+
+    case 1 :
+    console.log('Today is monday');
+    break;
+
+    case 2:
+    console.log('Today is tuesday');
+    break;
+
+    case 3 :
+    console.log('Today is wednesday');
+    break;
+
+    case 4 :
+    console.log('Today is thursday');
+    break;
+
+    case 5 :
+    console.log('Today is friday');
+    break;
+
+    case 6 :
+    console.log('Today is saturday');
+    break;
+
+   default : console.log('number of not a valid date');
+}
