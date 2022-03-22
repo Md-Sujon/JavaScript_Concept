f3425b90d8f94488a53a2824504883fa076ef4c1
Md-Sujon <mdsujon829562@gmail.com>
2022-03-22 12:44:53 +0600

    lppo

---
+++ forLoop.js
@@ -0,0 +1,17 @@
+// for(let i = 0; i < 10 ; i++){
+//     console.log((i+1) + " Md Sujon");
+// }
+
+// for(let i = 0; i < 10 ; i++){
+//     if(i %2==0){
+//         console.log(i);
+//     }
+// }
+let sum = 0;
+for (let i = 0; i< 10; i++){
+    console.log( sum + " + " + i + " = " + (sum+i));
+    sum = sum+i;
+    
+}
+console.log(sum);
+
